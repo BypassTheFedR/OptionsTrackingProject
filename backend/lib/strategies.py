8eeea4960b545f2b59daed5e0aa6ca66a7fd8bda
BypassTheFedR <joshuaRthornburg@gmail.com>
2024-03-23 09:04:47 -0700

    Commit message

--- backend/lib/strategies.py
+++ backend/lib/strategies.py
@@ -1,46 +1,74 @@
-from backend.lib.connect import connect_to_database, disconnect_from_database
-from datetime import datetime, date
-import pandas as pd
+from sqlalchemy import create_engine, Column, Integer, String, Float, Boolean, update
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-class Strategies:
-    # To calculate ROI
-    def __init__(self, underlying, initial_investment, total_premium_received, total_contracts, active=True):
+# from backend.lib.connect import connect_to_database, disconnect_from_database
+
+# from datetime import datetime, date
+# import pandas as pd
+
+# Create the SQLAlchemy engine
+engine = create_engine('sqlite:///backend/data/trading.db')
+
+# Create a session maker to interact with the database
+Session = sessionmaker(bind=engine)
+
+# Create a base class for declarative class definition
+Base = declarative_base()
+
+class Strategies(Base):
+    __tablename__ = 'strategies'
+
+    id = Column(Integer, primary_key=True)
+    underlying = Column(String)
+    InitialInvestment = Column(Float)
+    TotalPremiumReceived = Column(Float)
+    TotalContracts  = Column(Integer)
+    TotalMoneyRisked = Column(Integer)
+    CalculatedAverageInvested = Column(Float)
+    active = Column(Boolean)
+
+    # Define relationship to the Trades table
+    # trades = relationship("Trade", back_populates="strategy")
+
+    # declare the class
+    def __init__(self, underlying, InitialInvestment, TotalPremiumReceived, TotalContracts, active=True):
         self.underlying = underlying
-        self.initial_investment = initial_investment
-        self.total_premium_received = total_premium_received
-        self.total_contracts = total_contracts
-        self.total_money_risked = initial_investment
-        self.calculated_average_invested = self.total_money_risked / self.total_contracts
+        self.InitialInvestment = InitialInvestment
+        self.TotalPremiumReceived = InitialInvestment
+        self.TotalContracts = TotalContracts
+        self.TotalMoneyRisked = InitialInvestment
+        self.CalculatedAverageInvested = self.TotalMoneyRisked / self.TotalContracts
         self.active = active  
     
-    def add_strategy(self):
-        conn,cursor = connect_to_database()
-        cursor.execute("INSERT INTO Strategies (Underlying, InitialInvestment, TotalPremiumReceived, TotalContracts, TotalMoneyRisked, CalculatedAverageInvested, Active) VALUES (? ,? ,? ,? ,? ,? ,?)",
-                        (self.underlying, self.initial_investment, self.total_premium_received, self.total_contracts, self.total_money_risked, self.calculated_average_invested, self.active))
-        disconnect_from_database(conn, cursor)
-        
-def display_active_strategies():
-        conn, cursor = connect_to_database()
-        cursor.execute("SELECT * FROM Strategies WHERE Active=True")
-        active_strategies = cursor.fetchall()
-        df = pd.DataFrame(active_strategies, columns=["StrategyID", "Underlying", "InitialInvestment", "TotalPremiumReceived", "TotalContracts", "TotalMoneyRisked", "CalculatedAverageInvested","Active"])
-        print(df)
-        disconnect_from_database(conn, cursor)
+    @classmethod
+    def add_strategy(cls, session, underlying, initial_investment, TotalPremiumReceived,TotalContracts,active=True):
+        strategy = cls(underlying,initial_investment,TotalPremiumReceived,TotalContracts,active=True)
+        session.add(strategy)
+        session.commit()
 
-def display_closed_strategies():
-        conn, cursor = connect_to_database()
-        cursor.execute("SELECT * FROM Strategies WHERE Active=False")
-        active_strategies = cursor.fetchall()
-        df = pd.DataFrame(active_strategies, columns=["StrategyID", "Underlying", "InitialInvestment", "TotalPremiumReceived", "TotalContracts", "TotalMoneyRisked", "CalculatedAverageInvested","Active"])
-        print(df)
-        disconnect_from_database(conn, cursor)
+    @classmethod    
+    def display_active_strategies(cls, session):
+         active_strategies = session.query(cls).filter(cls.active == True).all()
+         return active_strategies
+    
+    @classmethod    
+    def display_closed_strategies(cls, session):
+         closed_strategies = session.query(cls).filter(cls.active == False).all()
+         return closed_strategies
+    
+    @classmethod
+    def close_strategy(cls, session, strategy_id):
+        close_strategy = update(Strategies).where(Strategies.id == strategy_id).values(active = False)
+        session.execute(close_strategy)
+        session.commit()
 
-##############################################################################
-# Working on this function ChatGPT Ignore def close_strategy() for now       #
-##############################################################################
-# def close_strategy(strategy_ID, Active=False):
-#      conn, cursor = connect_to_database()
-#      cursor.execute("SELECT Open FROM Trades WHERE StrategyID = ?", (strategy_ID,))
-#      trade_status = cursor.fetchall()
-#      if trade_status == True:# check all trades are closed before closing strategy
-#            cursor.execute("UPDATE Stategies SET ClosingPremium = ?, TotalPremium = ?, Open = ? WHERE TradeID = ?", (closing_prem, total_prem, open, trade_ID) "
+session = Session()
+
+# Add a new strategy
+Strategies.add_strategy(session, 'AAPL', 10000.0, 500.0, 5)
+
+# Display active strategies
+active_strategies = Strategies.display_active_strategies(session)
+for strategy in active_strategies:
+    print(strategy.id, strategy.underlying, strategy.initial_investment)
